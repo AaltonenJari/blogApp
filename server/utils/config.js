db0b64d295e0308c3d9fa87d21e874944990bb9b
AaltonenJari <jaritapani.aaltonen@gmail.com>
2026-07-28 18:46:20 +0300

    Samanaikainen käynnistys kuntoon

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,8 +1,13 @@
-require('dotenv').config()
+const path = require('path')
+const dotenv = require('dotenv')
 
-const PORT = process.env.PORT
-const MONGODB_URI = process.env.NODE_ENV === 'test' 
-  ? process.env.TEST_MONGODB_URI
+const serverEnvPath = path.resolve(__dirname, '../.env')
+
+dotenv.config({ path: serverEnvPath })
+
+const PORT = process.env.PORT || 3003
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI || process.env.MONGODB_URI
   : process.env.MONGODB_URI
 
 module.exports = { MONGODB_URI, PORT }
